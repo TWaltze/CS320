54865206801d622fdc642270441f34118bfbf5c0
Tyler Waltze <tylerwaltze@gmail.com>
2014-11-29 21:55:36 -0800

    Homework #5, Problem #1 Solutions

--- hw5/Database.hs
+++ hw5/Database.hs
@@ -27,15 +27,38 @@ example = [
     Allow (User "Alice", Table "Revenue")
   ]
 
+badExample = [
+        Add (User "Bob"),
+        Create (Table "Revenue"),
+        Insert (Table "Revenue", [("Day", 1), ("Amount", 2400)]),
+        Insert (Table "Revenue", [("Day", 2), ("Amount", 1700)]),
+        Insert (Table "Revenue", [("Day", 3), ("Amount", 3100)]),
+        Allow (User "Alice", Table "Revenue")
+    ]
+
+badExample' = [
+        Add (User "Alice"),
+        Add (User "Bob"),
+        Insert (Table "Revenue", [("Day", 1), ("Amount", 2400)]),
+        Insert (Table "Revenue", [("Day", 2), ("Amount", 1700)]),
+        Insert (Table "Revenue", [("Day", 3), ("Amount", 3100)]),
+        Allow (User "Alice", Table "Revenue")
+    ]
+
 -- Useful function for retrieving a value from a list
 -- of (label, value) pairs.
 lookup' :: Column -> [(Column, Integer)] -> Integer
 lookup' c' ((c,i):cvs) = if c == c' then i else lookup' c' cvs
 
 -- Complete for Assignment 5, Problem 1, part (a).
+isAllowed commands user table = length [u | Allow (u, t) <- commands, u == user, t == table] > 0
+
 select :: [Command] -> User -> Table -> Column -> Maybe [Integer]
-select _ _ _ _ =
-  Nothing
+select commands user table column =
+    if isAllowed commands user table == True then
+        Just [lookup' column d | Insert (t, d) <- commands, table == t]
+    else
+        Nothing
 
 
 
@@ -43,16 +66,29 @@ select _ _ _ _ =
 type Operator = Integer -> Integer -> Integer
 
 -- Complete for Assignment 5, Problem 1, part (b).
+fromJust :: Maybe a -> a
+fromJust (Just x) = x
+
 aggregate :: [Command] -> User -> Table -> Column -> Operator -> Maybe Integer
-aggregate _ _ _ _ _ =
-  Nothing
+aggregate commands user table column operator =
+    if isAllowed commands user table == True then
+        Just (foldr operator 0 (fromJust (select commands user table column)))
+    else
+        Nothing
 
 
 
 -- Complete for Assignment 5, Problem 1, part (c).
+valid (Allow (user, table)) commands =
+    length [u | Add (u) <-commands, u == user] > 0 &&
+    length [t | Create (t) <-commands, t == table] > 0
+valid (Insert (table, _)) commands =
+    length [t | Create (t) <-commands, t == table] > 0
+valid _ commands = True
+
 validate :: [Command] -> Bool
-validate _ =
-  False
+validate commands =
+    length [command | command <- reverse commands, valid command commands == False] == 0
 
 
 
