5fd46c999a3158a5e1c62e01afbff66c5e98fba3
Tyler Waltze <tylerwaltze@gmail.com>
2014-11-29 22:44:08 -0800

    Homework #5, Problem #2a Solution

--- hw5/Allocation.hs
+++ hw5/Allocation.hs
@@ -13,11 +13,20 @@ type Bin = Integer
 data Alloc = Alloc Bin Bin deriving (Eq, Show)
 
 data Graph =
-    Branch Alloc Graph Graph 
+    Branch Alloc Graph Graph
   | Finish Alloc
   deriving (Eq, Show)
 
 type Strategy = Graph -> Graph
+
+
+
+graph :: Alloc -> [Item] -> Graph
+graph (Alloc x y) items =
+    if null (tail items) == False then
+        Branch (Alloc x y) ( graph (Alloc (x + head items) y) (tail items) ) ( graph (Alloc x (y + head items)) (tail items) )
+    else
+        Branch (Alloc x y) (Finish (Alloc (x + head items) y)) (Finish (Alloc x (y + head items)))
 
 
 
