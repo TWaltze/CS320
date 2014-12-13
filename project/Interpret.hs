6f62b40ce222d297348c828fa3fdc8fe3a9e88ec
Tyler Waltze <tylerwaltze@gmail.com>
2014-12-13 17:25:04 -0500

    Project problem 1b solutions

--- project/Interpret.hs
+++ project/Interpret.hs
@@ -5,13 +5,22 @@ import Parse
 import TypeCheck
 
 eval :: [(String, Bool)] -> Exp -> Bool
-eval _ _ = False -- Implement for Problem #1, part (b).
+eval env (Value v) = v
+eval env (Not e) = not (eval env e)
+eval env (And e1 e2) = (eval env e1) && (eval env e2)
+eval env (Or e1 e2) = (eval env e1) || (eval env e2)
+eval env (Variable x) = lookup' x env
 
 exec :: [(String, Bool)] -> Stmt -> ([(String, Bool)], Output)
 exec env (Print    e s) =
   let (env', o) = exec env s
   in (env', [eval env e] ++ o)
-exec env _ = (env, []) -- Implement the Assign and End cases for Problem #1, part (b).
+exec env (Assign x e s) =
+    let v = eval env e
+        env' = env ++ [(x, v)]
+        (env'', o) = exec env' s
+    in (env'', o)
+exec env _ = (env, [])
 
 interpret :: Stmt -> Maybe Output
 interpret _ = Nothing -- Implement for Problem #1, part (d).
