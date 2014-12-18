76df41c10ed809bd3dd726f9a2dfc03930dc29ff
Tyler Waltze <tylerwaltze@gmail.com>
2014-12-17 19:56:12 -0500

    Project problem 4b near-solution
    Flight from BOS -> SFO gave me time to figure it out (I think).
    
    Only thing left is to figure out how to implement a logical “or” using
    only logical “nand”.

--- project/Compile.hs
+++ project/Compile.hs
@@ -9,29 +9,63 @@ class Compilable a where
 
 instance Compilable Stmt where
     comp xrs (End) = STOP (Register 0)
-    -- comp xrs (Print e s) = STOP (Register -1)
-    -- comp xrs (Assign x e s) = STOP (Register -1)
+    comp xrs (Print e s) =
+        let expression = comp xrs e
+        in  expression +++
+            (COPY (Register 0) (result expression)
+            (comp xrs s))
+    comp xrs (Assign x e s) =
+        let expression = comp xrs e
+        in  expression +++
+            (comp ((x, (result expression)):xrs) s)
 
 instance Compilable Exp where
     comp xrs (Variable x) = STOP (lookup' x xrs)
-    -- comp xrs (Value b) =
-    --     let next = (topOfStack xrs) + 1
-    --     in if b == False then
-    --         INIT (next) ()
-    --     else
-    --         INIT (next) (FLIP next ())
-    comp xrs (Not e) = comp xrs e
+    comp xrs (Value b) =
+        let next = (topOfStack xrs) + 1
+        in if b == False then
+            INIT (next)
+            (STOP next)
+        else
+            INIT (next)
+            (FLIP next
+            (STOP next))
+    comp xrs (Not e) =
+        let expression = comp xrs e
+        in  expression +++
+            (FLIP (result expression)
+            (STOP (result expression)))
+    comp xrs (And e1 e2) =
+        let expression1 = comp xrs e1
+            expression2 = comp xrs e2
+            r1 = result expression1
+            r2 = result expression2
+        in  expression1 +++
+            expression2 +++
+            (NAND r1 r2 (r2 + 1)
+            (FLIP (r2 + 1)
+            (STOP (r2 + 1))))
+    -- comp xrs (Or e1 e2) =
+    --     let expression1 = comp xrs e1
+    --         expression2 = comp xrs e2
+    --         r1 = result expression1
+    --         r2 = result expression2
+    --     in  expression1 +++
+    --         expression2 +++
+    --         (NAND r1 r2 (r2 + 1)
+    --         (FLIP (r2 + 1)
+    --         (STOP (r2 + 1))))
 
 
 topOfStack :: [(Var, Register)] -> Register
 topOfStack xrs = maximum [r | (x, r) <- xrs]
 
--- result :: Instruction -> Register
--- result (STOP r) = r
--- result (INIT _ i) = result i
--- result (FLIP _ i) = result i
--- result (COPY _ _ i) = result i
--- result (NAND _ _ _ i) = result i
+result :: Instruction -> Register
+result (STOP r) = r
+result (INIT _ i) = result i
+result (FLIP _ i) = result i
+result (COPY _ _ i) = result i
+result (NAND _ _ _ i) = result i
 
 
 -- compileMin :: Stmt -> Maybe Instruction
